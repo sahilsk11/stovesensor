37bcfff92e7e87f8841c8a071e1c0298c18569d8
Sahil Kapur <sahilkapur.a@gmail.com>
2017-08-01 20:58:16 -0700

    Changed notification system and added new device option

--- aws_processing/scripts/data_storage.py
+++ aws_processing/scripts/data_storage.py
@@ -5,6 +5,11 @@ print "Content-type: application/json\n\n"
 import shelve
 import json
 import cgi
+import notification
+
+def send_notifications(code, users):
+    for user in users:
+        notification.send_notification(str(user["number"]), code)
 
 stovesensor_data = shelve.open("stove_info.shelve", writeback= True)
 if (not "devices" in stovesensor_data):
@@ -18,6 +23,8 @@ data = form.getfirst("data", "")
 if (command == "upload"):
     json_data = eval(data)
     stovesensor_data["devices"][code] = json_data
+    if (json_data["notification"]):
+        send_notifications(code, json_data["numbers"])
     print {"success":True}
     print(stovesensor_data)
     
@@ -25,5 +32,20 @@ if (command == "pageload"):
     data = stovesensor_data["devices"][code]
     j = json.dumps(data)
     print j
+    
+if (command == "newdevice"):
+    completed = False
+    while (not completed):
+        uid = 0
+        for i in range(0, 4):
+            number = random.randint(1, 9)
+            uid = uid * 10 + number
+        if (uid not in stovedata["devices"]):
+            stovedata["devices"][uid] = {}
+            completed = True
+    d = {"success": True, "code":uid}
+    j = json.dumps(d)
+    print j
+    
     
 stovesensor_data.close()
