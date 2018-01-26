121638d97b7008d68b81bd4505c96362896bc109
Sahil Kapur <sahilkapur.a@gmail.com>
2018-01-25 23:31:26 -0800

    Added missing letter

--- scripts/ajax.py
+++ scripts/ajax.py
@@ -40,14 +40,14 @@ if (code == None or code == ""):
         code = shelf["uid"]
 
 if (command == "pageload"):
-    temperature = current.fetch_value("temperatures", "temperature", 1)
-    status = current.fetch_value("calculated", "status", 1)
+    temperature = current.fetch_values("temperatures", "temperature", 1)
+    status = current.fetch_values("calculated", "status", 1)
     if (status == "ON"):
-        on_time = current.fetch_value("calculated", "time", 1)
+        on_time = current.fetch_values("calculated", "time", 1)
         on_time = on_time.strftime("%I:%M %p")
     else:
         on_time = "none"
-    time = current.fetch_value("temperatures", "time", 1)
+    time = current.fetch_values("temperatures", "time", 1)
     time = time.strftime("%I:%M %p on %m/%d/%y")
     d = {"temperature":temperature, "status": status, "on_time":on_time, "update_time":time, "code":code}
     j = json.dumps(d)
